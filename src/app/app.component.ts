0d5c5ffc0b8fc1c6c84f162cae5be6172e2a0824
ydeepakb <ydeepakb@gmail.com>
2018-01-14 12:06:59 +0530

    stage changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit {
   title = 'app';
+  ngOnInit() {
+    // $('.btn-expand-collapse').click(function(e) {
+    //   $('.navbar-primary').toggleClass('collapsed');
+    // });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { PageModule} from './pages/page.module';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app.routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent, SideMenuComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule, BrowserModule, PageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
